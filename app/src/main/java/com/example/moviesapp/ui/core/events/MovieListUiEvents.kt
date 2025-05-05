1c38cb5abb69865e0945cc05820bc55ca610ee73
girish goradiya <girishg.etechmavens@gmail.com>
2025-05-05 18:52:06 +0530

    - removing redundant code

--- app/src/main/java/com/example/moviesapp/ui/core/events/MovieListUiEvents.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moviesapp.ui.core.events
-
-sealed interface MovieListUiEvent {
-    data class Paginate(val category: String) : MovieListUiEvent
-}
--- app/src/main/java/com/example/moviesapp/ui/core/route/Screen.kt
+++ app/src/main/java/com/example/moviesapp/ui/core/route/Screen.kt
@@ -2,8 +2,6 @@ package com.example.moviesapp.ui.core.route
 
 sealed class Screen(val route:String) {
     data object Home: Screen("main")
-    data object PopulateMovieList : Screen("popularMovie")
-    data object UpcomingMovieList : Screen("upcomingMovie")
     data object Details : Screen("details")
     data object UserScreen:Screen("userScreen")
 }
